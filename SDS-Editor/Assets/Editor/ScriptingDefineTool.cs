e3562c18af8ff138b6ef8eec550121413550072f
Dave <ddawid@windowslive.com>
2020-08-30 16:44:28 +0200

    Import icons Create simple style for define data

---
+++ SDS-Editor/Assets/Editor/ScriptingDefineTool.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SDS.Data;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SDS.Editor
+{
+    public class ScriptingDefineTool : EditorWindow
+    {
+        private List<ScriptingDefineSymbolDataModel> _defines;
+        
+        [MenuItem("Tools/ScriptingDefineTool")]
+        public static void ShowExample()
+        {
+            ScriptingDefineTool wnd = GetWindow<ScriptingDefineTool>();
+            wnd.minSize = new Vector2(400, 300);
+            wnd.titleContent = new GUIContent("ScriptingDefineTool");
+        }
+
+        public void OnEnable()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+            
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/ScriptingDefineTool.uxml");
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/ScriptingDefineTool.uss");
+            
+            VisualElement uxmlRoot = visualTree.CloneTree();
+            root.Add(uxmlRoot);
+            root.styleSheets.Add(styleSheet);
+        }
+
+        private void OnGUI()
+        {
+            rootVisualElement.Q<VisualElement>("Container").style.height = new StyleLength(position.height);
+        }
+
+        private List<ScriptingDefineSymbolDataModel> LoadDefines()
+        {
+            var defines = AssetDatabase.LoadAllAssetsAtPath("Assets/Scripts/Data/").ToList();
+            return defines.OfType<ScriptingDefineSymbolDataModel>().ToList();
+        }
+
+        private void PopulateList()
+        {
+            _defines = LoadDefines();
+            if (_defines == null || _defines.Count == 0)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void ApplyDefines()
+        {
+            foreach (var define in _defines)
+            {
+                switch (Application.platform)
+                {
+                    case RuntimePlatform.OSXPlayer:
+                        break;
+                    case RuntimePlatform.WindowsPlayer:
+                        break;
+                    case RuntimePlatform.IPhonePlayer:
+                        break;
+                    case RuntimePlatform.Android:
+                        break;
+                    case RuntimePlatform.LinuxPlayer:
+                        break;
+                    case RuntimePlatform.WebGLPlayer:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SDS-Editor/Assets/Scripts/Data/ScriptingDefineSymbolDataModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace SDS.Data
+{
+    [Flags]
+    public enum Platforms
+    {
+        WindowsStandalone = 1,
+        MacStandalone = 2,
+        LinuxStandalone = 4,
+        Android = 8,
+        Ios = 16,
+        WebGL = 32
+        
+    }
+    [CreateAssetMenu(fileName = "ScriptingDefine")]
+    public class ScriptingDefineSymbolDataModel : ScriptableObject
+    {
+        public string ScriptingDefineSymbol { get; private set; }
+        public Platforms Platforms { get; private set; }
+        public bool EnableScriptingDefineSymbol { get; private set; }
+
+        [SerializeField] private string scriptingDefineSymbol;
+        [SerializeField] private Platforms platforms;
+        [SerializeField] private bool enableScriptingDefineSymbol;
+
+        public void SetScriptingDefineSymbol(string symbol)
+        {
+            scriptingDefineSymbol = symbol;
+            
+        }
+    }
+}
